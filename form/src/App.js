c44e7a434dff891e9bf9a282a9bb9568a73f2542
DESKTOP-3DPFR2G\daisu <daisuke.matsumoto@mediwill.co.jp>
2022-01-10 23:40:51 +0900

    テキスト入力フォーム

---
+++ form/src/App.js
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+const InputText = () => {
+  const [inputTextValue, setInputTextValue] = useState("");
+
+  const [text, setText] = useState("JavaScript");
+
+  const handleChange = (e) => setInputTextValue(e.target.value);
+
+  const handleClick = () => {
+    setText(inputTextValue);
+    setInputTextValue("");
+  };
+
+  return (
+    <div className="App">
+      <h1>I love {text}!!</h1>
+      <input type="text" value={inputTextValue} onChange={handleChange} />
+      <input type="button" value="入力" onClick={handleClick} />
+    </div>
+  );
+};
+
+export default function App() {
+  return <InputText />;
+}
