8c93b7bcdbdab6506d968f3b3c061897accb35cf
DESKTOP-3DPFR2G\daisu <daisuke.matsumoto@mediwill.co.jp>
2022-01-10 23:51:26 +0900

    select

--- form/src/App.js
+++ form/src/App.js
@@ -21,6 +21,29 @@ const InputText = () => {
   );
 };
 
+const InputSelectBox = () => {
+  const [selectedValue, setSelectedValue] = useState("HTML");
+
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <p>
+        現在選択されている値：
+        <b>{selectedValue}</b>
+      </p>
+
+      <select value={selectedValue} onChange={handleChange}>
+        <option value="HTML">HTML</option>
+        <option value="CSS">CSS</option>
+        <option value="JavaScript">JavaScript</option>
+      </select>
+    </div>
+  );
+}
+
 export default function App() {
-  return <InputText />;
+  return <InputSelectBox />;
 }
